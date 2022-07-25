10345d102036f4593bf9ed4fe8e5a6b7df819245
Asmaa-Mansour <97177972+Asmaa-Mansour@users.noreply.github.com>
2022-07-25 13:15:57 +0200

    Update main_depts.py

--- main_depts.py
+++ main_depts.py
@@ -11,10 +11,10 @@ import datetime
 from DEPTS.utils import TimeseriesSampler, to_tensor, warm_PM_parameters_perK
 from DEPTS.utils import default_device, mape_loss, mase_loss, smape_2_loss
 from DEPTS.models import PeriodicityModule, depts_expansion_general
-from datasets.electricity import ElectricityDataset, ElectricityMeta
-from datasets.traffic import TrafficDataset, TrafficMeta
-from datasets.caiso import CaisoDataset, CaisoMeta
-from datasets.nordpool import ProductionDataset, ProductionMeta
+from DEPTS.datasets.electricity import ElectricityDataset, ElectricityMeta
+from DEPTS.datasets.traffic import TrafficDataset, TrafficMeta
+from DEPTS.datasets.caiso import CaisoDataset, CaisoMeta
+from DEPTS.datasets.nordpool import ProductionDataset, ProductionMeta
 
 def setup_seed(seed):
     random.seed(seed)
